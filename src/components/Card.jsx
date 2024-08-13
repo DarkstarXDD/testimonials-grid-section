6b6ae014597c312279885726a0eedb3350307d76
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-08-13 09:08:07 +0530

    Update grid layout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,12 @@
+import classNames from "classnames"
+
 export default function Card({ id, avatar, name, ...rest }) {
+  let gridColSpanClass = id === 1 || id === 4 ? "grid-col-span-2" : ""
+
+  const cardClassNames = classNames("card", gridColSpanClass)
+
   return (
-    <div className={`card ${id === 1 || id === 4 ? "grid-col-span-2" : ""}`}>
+    <div className={cardClassNames}>
       <div className="card__profile-section">
         <img src={avatar} alt="" className="card__avatar" />
         <p className="card__name">{name}</p>
