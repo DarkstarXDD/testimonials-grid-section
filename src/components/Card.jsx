2a536d74523ae7eef9b5f205e4d017152c530014
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-08-14 21:08:07 +0530

    Update animation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -23,25 +23,27 @@ export default function Card({ id, avatar, name, ...rest }) {
   }
 
   return (
-    <motion.div
-      className={cardClassNames}
-      ref={ref}
-      variants={isReducedMotion ? reducedMotionVariants : fadeInVariants}
-      initial="hidden"
-      animate={isInView ? "visible" : "hidden"}
-      transition={{ duration: 0.5 }}
-    >
-      <div className="card__profile-section">
-        <img src={avatar} alt="" className="card__avatar" />
-        <p className="card__name">{name}</p>
-        <p className="card__verified-status">
-          {rest.isVerified ? "Verified Graduate" : "Student"}
-        </p>
-      </div>
-      <h2 className="card__heading">{rest.title}</h2>
-      <blockquote className="card__testimonial">
-        {`” ${rest.testimonial} ”`}
-      </blockquote>
-    </motion.div>
+    <div className={cardClassNames}>
+      <motion.div
+        className="card__text-wrapper"
+        ref={ref}
+        variants={isReducedMotion ? reducedMotionVariants : fadeInVariants}
+        initial="hidden"
+        animate={isInView ? "visible" : "hidden"}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="card__profile-section">
+          <img src={avatar} alt="" className="card__avatar" />
+          <p className="card__name">{name}</p>
+          <p className="card__verified-status">
+            {rest.isVerified ? "Verified Graduate" : "Student"}
+          </p>
+        </div>
+        <h2 className="card__heading">{rest.title}</h2>
+        <blockquote className="card__testimonial">
+          {`” ${rest.testimonial} ”`}
+        </blockquote>
+      </motion.div>
+    </div>
   )
 }
